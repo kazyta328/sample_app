9b4a15eef4fe7ed51e1620e9ecc395104e02a924
Kazuyuki Taniguchi <kazyta328@gmail.com>
2017-11-10 17:49:45 +0900

    replace

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # 保存の成功をここで扱う
+      flash[:success] = "Welcom to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
